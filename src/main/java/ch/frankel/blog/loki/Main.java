c0c977bd04cf29ed42d7ad9dcd39112e48dbf55e
Nicolas Fränkel <nicolas@frankel.ch>
2023-07-15 19:41:37 +0200

    Replace direct call with Logback appender

--- src/main/java/ch/frankel/blog/loki/Main.java
+++ src/main/java/ch/frankel/blog/loki/Main.java
@@ -1,44 +1,21 @@
 package ch.frankel.blog.loki;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.text.MessageFormat;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 
-import static java.util.concurrent.TimeUnit.NANOSECONDS;
-import static java.util.concurrent.TimeUnit.SECONDS;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
-        var template = """
-                '{'
-                  "streams": [
-                    '{'
-                      "stream": '{'
-                        "app": "{0}"
-                      '}',
-                      "values": [
-                        [ "{1}", "{2}" ]
-                      ]
-                    '}'
-                  ]
-                '}'""";
-        var now = LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant();
-        var nowInEpochNanos = NANOSECONDS.convert(now.getEpochSecond(), SECONDS) + now.getNano();
-        var payload = MessageFormat.format(template, "demo", String.valueOf(nowInEpochNanos), "Hello from Java App");
-        System.out.println(payload);
-        var request = HttpRequest.newBuilder()
-                .uri(new URI("http://localhost:3100/loki/api/v1/push"))
-                .header("Content-Type", "application/json")
-                .POST(HttpRequest.BodyPublishers.ofString(payload))
-                .build();
-        HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response);
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class.toString());
+
+    public static void main(String[] args) throws InterruptedException {
+        String who;
+        if (args.length == 0) {
+            who = "Java app";
+        } else {
+            who = args[0];
+        }
+        LOGGER.info("Hello from {}!", who);
+        Thread.sleep(5000);
     }
 }
